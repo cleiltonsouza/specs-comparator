bd32821d61f88a0226f941313c2492697c58d5e0
cleiltonsouza <cleiltonsouza@sensedia.com>
2023-02-28 15:55:39 -0300

    Basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import SwaggerUI from 'swagger-ui'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['../../node_modules/swagger-ui/dist/swagger-ui.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'specs-comparator';
+  @ViewChild('left_swagger_ui') leftSwaggerViewContainer!: ElementRef
+  @ViewChild('right_swagger_ui') rightSwaggerViewContainer!: ElementRef
+
+  ngAfterViewInit() {
+
+    SwaggerUI({
+      domNode: this.leftSwaggerViewContainer?.nativeElement,
+      url: 'https://br-openinsurance.github.io/areadesenvolvedor/files/swagger/capitalization-title.yaml'
+    })
+    SwaggerUI({
+      domNode: this.rightSwaggerViewContainer?.nativeElement,
+      url: 'https://sensedia.github.io/draft-openapi/swagger-apis/capitalization-bonds/1.0.0-rc1.0.yml'
+    })
+  }
 }
