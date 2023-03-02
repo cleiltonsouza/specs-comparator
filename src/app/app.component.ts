635b65cf6845f670c39a62d060ee0abe9d0e9515
cleiltonsouza <cleiltonsouza@sensedia.com>
2023-03-02 15:58:36 -0300

    Select specs dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,85 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import SwaggerUI from 'swagger-ui'
+import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist'
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['../../node_modules/swagger-ui/dist/swagger-ui.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit {
   title = 'specs-comparator';
   @ViewChild('left_swagger_ui') leftSwaggerViewContainer!: ElementRef
   @ViewChild('right_swagger_ui') rightSwaggerViewContainer!: ElementRef
 
-  ngAfterViewInit() {
+  openFinanceApis: any[] = []
+  openInsuranceApis: { name: string, url: string }[] = []
+  opinSelectedAPI: any
+  opfSelectedAPI: any
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getOpenFinanceSpecs()
+    this.getOpenInsuranceSpecs()
+  }
+
+  async getOpenInsuranceSpecs() {
+    const data = await lastValueFrom(this.http.get<any[]>("https://api.github.com/repos/br-openinsurance/areadesenvolvedor/contents/documentation/source/files/swagger"))
+
+    data.forEach(element => {
+      this.openInsuranceApis.push({ name: element.name, url: element.download_url })
+    });
+  }
+
+  async getOpenFinanceSpecs() {
+
+    var apiNames: string[] = []
+
+    const data = await lastValueFrom(this.http.get<any[]>("https://api.github.com/repos/Openbanking-brasil/openapi/contents/swagger-apis"))
+
+    data.forEach(element => {
+      apiNames.push(element.name)
+    });
+    await this.getOpenFinanceSpecsUrls(apiNames)
+  }
+
+  async getOpenFinanceSpecsUrls(apiNames: string[]) {
+
+    for (const name of apiNames) {
+      const data = await lastValueFrom(this.http.get<any[]>(`https://api.github.com/repos/Openbanking-brasil/openapi/contents/swagger-apis/${name}`))
+
+      var apiVersions: any[] = []
+      data.forEach(element => {
+        apiVersions.push({ name: element.name, url: element.download_url })
+      });
+
+      apiVersions = apiVersions.filter(value => value.url.slice(-4) != "html")
+
+      this.openFinanceApis.push({ apiName: name, specs: apiVersions })
+    }
+  }
+
+
+  loadRightSwaggerView(value: any) {
+    console.log("the value is", value.target.value)
+    console.log(this.opinSelectedAPI)
 
     SwaggerUI({
-      domNode: this.leftSwaggerViewContainer?.nativeElement,
-      url: 'https://br-openinsurance.github.io/areadesenvolvedor/files/swagger/capitalization-title.yaml'
-    })
-    SwaggerUI({
       domNode: this.rightSwaggerViewContainer?.nativeElement,
-      url: 'https://sensedia.github.io/draft-openapi/swagger-apis/capitalization-bonds/1.0.0-rc1.0.yml'
+      presets: [SwaggerUIBundle['presets'].apis, SwaggerUIStandalonePreset],
+      layout: "StandaloneLayout",
+      url: this.opinSelectedAPI.url
+    })
+  }
+
+  loadLeftSwaggerView() {
+    SwaggerUI({
+      domNode: this.leftSwaggerViewContainer?.nativeElement,
+      presets: [SwaggerUIBundle['presets'].apis, SwaggerUIStandalonePreset],
+      layout: "StandaloneLayout",
+      urls: this.opfSelectedAPI.specs
     })
   }
 }
